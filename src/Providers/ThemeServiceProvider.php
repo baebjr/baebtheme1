70bd463eb4ddacccdde6c21318270ce03e027d41
Johannes Roth <johannes@badassebikes.com>
2019-11-22 13:05:44 +0100

    leading tabs removed

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -1,17 +1,17 @@
-    <?php
-     
-    namespace Theme\Providers;
-     
-    use Plenty\Plugin\ServiceProvider;
-     
-    class ThemeServiceProvider extends ServiceProvider
-    {
-     
-    	/**
-    	 * Register the service provider.
-    	 */
-    	public function register()
-    	{
-     
-    	}
-    }
+<?php
+ 
+namespace Theme\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class ThemeServiceProvider extends ServiceProvider
+{
+ 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+ 
+	}
+}
